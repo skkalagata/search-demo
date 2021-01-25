6427b3cd3ce69c39be7326ca3e8775474bbf8fd1
Sunilkumar Kalagata <sunilkumarkalagata@gmail.com>
2021-01-24 20:10:54 -0600

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+imports: [ProductsModule, 
+  MongooseModule.forRoot(
+    'mongodb+srv://myuser:C3L1a32mk72TIODZ@cluster0.7da4f.mongodb.net/demoData?retryWrites=true&w=majority'
+    )],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/products/products.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common'
+import { constants } from 'buffer'
+
+//import { title } from 'process';
+import { ProductsService } from './products.service'
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @Post()
+  async addProduct(
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ): Promise<any> {
+    const generatedId = await this.productsService.insertProduct(
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    )
+    return { id: generatedId }
+  }
+
+  @Get()
+  async getAllProducts() {
+    const products = await this.productsService.getProducts()
+    return products
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string) {
+    return this.productsService.getSingleProduct(prodId)
+  }
+
+  @Patch(':id')
+  async updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    await this.productsService.updateProduct(
+      prodId,
+      prodTitle,
+      prodDesc,
+      prodPrice,
+    )
+    return null
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') prodId: string) {
+    this.productsService.deleteProduct(prodId)
+    return null
+  }
+}
